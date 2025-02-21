5aa6d38d2c1be34a643b02cededd19d82af3c164
George Roudebush <roudebush.g@northeastern.edu>
2025-02-21 11:18:51 -0500

    update configuration parsing to be more flexible

--- src/compression.py
+++ src/compression.py
@@ -13,8 +13,10 @@ from tokenize import String
 
 import yaml
 from skimage import io
+import imageio.v3 as imageio
 import os
 from datetime import datetime
+import numpy as np
 import plotly.express as px
 
 class CompressImage:
@@ -29,9 +31,12 @@ class CompressImage:
         if not config:
             self.config = {}
             self.compression_function = None
+            print("No configuration provided")
         else:
-            self.config = config
+            with open(config,'r') as config_file:
+                self.config = yaml.load(config_file, yaml.SafeLoader)
             self.update_configuration(self.config)
+            # print(self.config)
 
 
 
@@ -52,20 +57,31 @@ class CompressImage:
             self.update_configuration(config)
 
         image_array = io.imread(raw_image_file)
-        return self.compression_function(image_array, config)
+        return self.compression_function(image_array, save_location=None, **self.config["compression_parameters"])
 
-    def jpeg_baseline(self, image_uncompressed, config, save_location=None):
+    def jpeg_baseline(self, image_uncompressed, save_location=None, **kwargs):
         """
         :param image_uncompressed: image as a numpy array
         :param save_location: if none is provided, will be saved to the tmp directory. If it is provided,
         omit the .jpeg extension from the filename.
         :return:
         """
-        if not save_location:
-            temp_location = os.path.join(os.getcwd(), "tmp", f"temp_img_{datetime.now().strftime("%Y%m%d_%H%M%S")}.jpeg")
-            io.imsave(temp_location,
-                      image_uncompressed)
-            return temp_location
+        if "quality_factor" in kwargs.keys():
+            quality_factor = kwargs["quality_factor"]
         else:
-            io.imsave(f"{save_location}.jpeg", image_uncompressed)
-            return f"{save_location}.jpeg"
+            quality_factor = 100
+
+        if image_uncompressed.dtype != np.uint8:
+            image_uncompressed = (255 * (image_uncompressed / np.max(image_uncompressed))).astype(np.uint8)
+
+        # Remove alpha channel if present (JPEG does not support transparency)
+        if image_uncompressed.shape[-1] == 4:
+            image_uncompressed = image_uncompressed[:, :, :3]
+
+        if not save_location:
+            file_save_location = os.path.join(os.getcwd(), "tmp", f"temp_img_{datetime.now().strftime("%Y%m%d_%H%M%S")}.jpeg")
+        else:
+            file_save_location = os.path.join(os.getcwd(),f"{save_location}.jpeg")
+        imageio.imwrite(file_save_location, image_uncompressed,
+                  quality=quality_factor)
+        return f"{save_location}.jpeg"
