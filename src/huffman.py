a41b63f3c9c0d7b5ecf1cebbafc9d4ec6a9fd04f
George Roudebush <roudebush.g@northeastern.edu>
2025-03-29 06:20:14 -0400

    add libraries to huffman.py

--- src/huffman.py
+++ src/huffman.py
@@ -1,3 +1,8 @@
+import numpy as np
+from collections import Counter, namedtuple
+import heapq
+
+
 zigzag_pattern = np.array([
     [ 0,  1,  5,  6, 14, 15, 27, 28],
     [ 2,  4,  7, 13, 16, 26, 29, 42],
